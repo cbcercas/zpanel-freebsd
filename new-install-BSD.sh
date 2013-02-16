e75d9a984087a697f36b526e0184a82894fcf42b
Christopher CERCAS <cbcercas@gmail.com>
2013-02-17 00:11:35 +0100

    fix get ip now curl not required

--- new-install-BSD.sh
+++ new-install-BSD.sh
@@ -13,7 +13,7 @@ tempfile3=`ZpanelX 2>/dev/null` || tempfile3=/tmp/ZpanelX_3_$$
 tempfile4=`ZpanelX 2>/dev/null` || tempfile4=/tmp/ZpanelX_4_$$
 touch $tempfile1 $tempfile2 $tempfile3 $tempfile4
 chmod 700 $tempfile1 $tempfile2 $tempfile3 $tempfile4
-IP=`curl ifconfig.me`
+IP=`fetch -q -o - http://checkip.dyndns.org | sed -e 's/[^:]*: //' -e 's/<.*$//'`
 
 trap "rm -f $tempfile1 $tempfile2 $tempfile3 $tempfile4" 0 1 2 5 15
 
